0214447f8b33ba41fa83576d6220e24679de6303
Deeve-hub <dmorl1312@gmail.com>
2023-05-01 16:04:13 -0400

    Added better unit test for health check

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,7 @@ use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run().await
+    //Bubble up the io::Error if we fail to bind
+    // Otherwise call await
+    run()?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,24 @@
-use zero2prod::main;
+#[tokio::test]
+async fn health_check_works() {
+    //Arrange
+    spawn_app();
+    //We need request
+    let client = reqwest::Client::new();
+    //Act
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request");
 
-#[test]
-fn dummy_test() {
-    main()
+    //Asert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+//Launch our stuff
+fn spawn_app() {
+    let server = zero2prod::run().expect("Failed to bind address");
+
+    let _ = tokio::spawn(server);
 }
